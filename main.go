0fc86415a42e0b9c4d5ee97b0b13476bdd6f8ea1
EmmaSalvi <emma.salvi@etu-webschoolfactory.fr>
2018-12-03 15:08:27 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"wsf/devopstrain/handler"
+)
+
+func main() {
+
+	// connection := os.Getenv("DATABASE_URL")
+
+	fmt.Println(os.Getenv("DATABASE_URL"))
+
+	port := os.Getenv("PORT") //OS
+	// variable port configurée par et pour Heroku
+	if port == "" {
+		port = "8080"
+	}
+
+	myHandler := handler.NewHandler()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: myHandler,
+		// ReadTimeout:  10 * time.Second,
+		// WriteTimeout: 10 * time.Second,
+	}
+
+	fmt.Println("Server is running at", port)
+
+	log.Fatal(server.ListenAndServe())
+}
